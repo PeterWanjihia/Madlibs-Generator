e602188d71155919aa96281153f8c693a3c5f38c
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-08-26 19:48:35 +0300

    made some changes in the singly linked list

--- file.py
+++ file.py
@@ -2,6 +2,9 @@
 # Singly Lists
 
 # Declare class
+from tkinter import N
+
+
 class Node:
     def __init__(self,data):
         self.data = data
@@ -12,12 +15,42 @@ class LinkedList:
     def __init__(self):
         self.head = None
         
+    def listLength(self):
+        currentNode = self.head
+        length = 0
+        while currentNode is not None:
+            length+=1
+            currentNode = currentNode.next
+        return length       
+            
+    
     def insertHead(self,newNode):
         temporaryNode = self.head
         self.head = newNode
-        self.head.next = temporaryNode
+        newNode.next = temporaryNode
         del temporaryNode
+
+    
+    def insertAt(self,newNode,position):
+        if position < 0 or position >self.listLength():
+            print('Invalid Position')
+            return
         
+        if position == 0:
+            self.insertHead(newNode)
+            return
+        currentNode = self.head
+        currentPosition = 0
+        while  True:
+            if currentPosition == position:
+                previousNode.next = newNode
+                newNode.next = currentNode
+                break
+            previousNode = currentNode
+            currentNode = currentNode.next
+            currentPosition +=1 
+            
+                   
     def insert(self,newNode):
         if self.head is None:
             self.head = newNode
@@ -44,9 +77,12 @@ firstNode = Node('John')
 secondNode = Node('Ben')
 thirdNode = Node('Matthew')
 fourthNode = Node('Peter')
+fifthNode = Node('Felix')
 linkedList.insert(firstNode)        
 linkedList.insert(secondNode)        
 linkedList.insert(thirdNode)  
 linkedList.insertHead(fourthNode)
+linkedList.insertAt(fifthNode,1)
 
-linkedList.printList()      
+linkedList.printList()  
+ 
