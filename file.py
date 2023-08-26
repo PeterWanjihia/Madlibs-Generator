ff0be8084cb6cca78d74e139a85eb3ae5873c2a1
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-08-26 12:50:13 +0300

    changes

--- file.py
+++ file.py
@@ -1,10 +1,52 @@
-import re
+# Linked Lists
+# Singly Lists
 
-def extract_words(text):
-    pattern = r'\[([^\]]+)\]'  # Regular expression pattern to match words inside square brackets
-    matches = re.findall(pattern, text)
-    return matches
+# Declare class
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+# Declare linkedlist class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def insertHead(self,newNode):
+        temporaryNode = self.head
+        self.head = newNode
+        self.head.next = temporaryNode
+        del temporaryNode
+        
+    def insert(self,newNode):
+        if self.head is None:
+            self.head = newNode
+        else:
+            lastNode = self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode = lastNode.next
+            lastNode.next = newNode
+            
+    def printList(self):
+        currentNode = self.head
+        print(currentNode)
+        while True:
+            if currentNode is None:
+                break
+            print(currentNode.data)
+            currentNode = currentNode.next
+            
+            
+linkedList = LinkedList()
+firstNode = Node('John')
+secondNode = Node('Ben')
+thirdNode = Node('Matthew')
+fourthNode = Node('Peter')
+linkedList.insert(firstNode)        
+linkedList.insert(secondNode)        
+linkedList.insert(thirdNode)  
+linkedList.insertHead(fourthNode)
 
-input_text = "Once upon a time, in a [adjective] [noun] far away, there lived a [adjective] [noun]."
-word_array = extract_words(input_text)
-print(word_array)
+linkedList.printList()      
